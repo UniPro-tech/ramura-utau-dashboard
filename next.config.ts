eafa30e355d0c582ac9b3c53acb2a395f66727ab
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-01 00:45:51 +0900

    miss

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "100gb",
+      bodySizeLimit: "10240mb",
     },
   },
   output: "standalone",
