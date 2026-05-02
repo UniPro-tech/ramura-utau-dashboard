033174fcc747c8702115d36aed819218f8989fe9
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-02 11:52:19 +0900

    miss

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     proxyClientMaxBodySize: "1gb",
+    serverActions: {
+      bodySizeLimit: "1gb",
+    },
   },
   output: "standalone",
   poweredByHeader: false,
